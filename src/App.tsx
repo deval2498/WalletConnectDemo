9191bd86e1460c67fc1873d44a44b472995a9e49
Deval Minocha <devalminocha@Devals-MacBook-Pro.local>
2025-01-31 17:20:22 +0530

    removing useless components

--- src/App.tsx
+++ src/App.tsx
@@ -26,15 +26,9 @@ export function App() {
   return (
     <div className={"pages"}>
       <img src="/reown.svg" alt="Reown" style={{ width: '150px', height: '150px' }} />
-      <h1>AppKit ethers React dApp Example</h1>
+      <h1>Contract Signer</h1>
           <appkit-button />
           <ActionButtonList/>
-          <div className="advice">
-            <p>
-              This projectId only works on localhost. <br/>
-              Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
-            </p>
-          </div>
           {/* <InfoList hash={transactionHash} signedMsg={signedMsg} balance={balance}/> */}
     </div>
   )
